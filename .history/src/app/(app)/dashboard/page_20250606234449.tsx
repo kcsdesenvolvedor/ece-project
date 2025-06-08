8a9a78742e8e795dbb84ba0f16ebb1430dc67b0a
kcsdesenvolvedor <kcs.desenvolvedor@gmail.com>
2025-06-08 16:01:55 -0300

    Remove .history do repositório

--- .history/src/app/(app)/dashboard/page_20250606234449.tsx
+++
@@ -1,9 +0,0 @@
-// app/(app)/dashboard/page.tsx
-export default function DashboardPage() {
-    return (
-      <div>
-        <h1 className="text-2xl font-bold">Dashboard</h1>
-        <p>Bem-vindo ao painel de administração!</p>
-      </div>
-    )
-  }
